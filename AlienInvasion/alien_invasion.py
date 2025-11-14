f2ad8822e641c8fdda11f12ebc12cfe01b0fc5a8
Jason-happy-ops <2941854208@qq.com>
2025-11-14 20:46:28 +0800

    完成12.6

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -19,25 +19,34 @@ class AlienInvasion:
     ##开始游戏主循环
         while True:
             self._check_events()    #指定变量名和要调用的方法
+            self.ship.update()  ##这是按下方向键，飞船持续移动的关键，相当于触发器
             self._update_screen()
-
+            self.clock.tick(60)
             #每次循环重绘屏幕
     def _check_events(self):
             ##侦听键盘和鼠标事件
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN:  #按下按键
                     if event.key == pygame.K_RIGHT:
                         ##向右移动飞船
-                        self.ship.rect.x+=1
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right =False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+
                         
     def _update_screen(self):
         ##更新屏幕上的
             self.screen.fill(self.settings.bg_color) ##重绘屏幕
             self.ship.blitme()
             pygame.display.flip()
-            self.clock.tick(60)
+            
 
     
 if __name__=='__main__':
