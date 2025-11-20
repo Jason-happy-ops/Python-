b50d147cb785b235ed485cdab1e83259c4d3550f
Jason-happy-ops <2941854208@qq.com>
2025-11-20 22:44:04 +0800

    完成问题1

---
+++ practise/random_run.py
@@ -0,0 +1,24 @@
+from random import choice
+
+class Randomwalk:
+    def __init__(self,num_points) -> None:
+        self.num_points=5000
+        #随机从原点开始,用两个列表分别储存x，y的坐标
+        self.x_value = [0]
+        self.y_value = [0]
+
+    def fill_work(self):
+        while len(self.x_values) < self.num_points:
+            x_direction=choice=([-1,1])  #用来表示向正or负坐标轴移动
+            x_distance=choice=([1,2,3,4])  #移动多少距离
+            x_move=x_direction * x_distance
+
+            y_direction=choice=([-1,1])  #用来表示向正or负坐标轴移动
+            y_distance=choice=([1,2,3,4])  #移动多少距离
+            y_move=y_direction * y_distance
+
+            
+
+            
+
+
